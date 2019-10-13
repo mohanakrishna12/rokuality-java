1216b5cb96196878cc93e0ce40a7fc14a71709e2
Brandon Clark <baclark77@Brandons-MacBook-Pro-2.local>
2019-10-12 22:04:06 -0400

    xbox testing cleanup

--- src/test/java/com/rokuality/test/tests/XBoxTests.java
+++ src/test/java/com/rokuality/test/tests/XBoxTests.java
@@ -4,21 +4,13 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.io.File;
 import java.util.Arrays;
-import java.util.List;
 
 import com.rokuality.core.driver.By;
 import com.rokuality.core.driver.DeviceCapabilities;
 import com.rokuality.core.driver.Element;
-import com.rokuality.core.driver.ScreenText;
-import com.rokuality.core.driver.roku.RokuDeviceInfo;
-import com.rokuality.core.driver.roku.RokuDriver;
 import com.rokuality.core.driver.xbox.XBoxDriver;
-import com.rokuality.core.enums.RokuButton;
-import com.rokuality.core.enums.XBoxButton;
 import com.rokuality.core.exceptions.NoSuchElementException;
 import com.rokuality.core.exceptions.ServerFailureException;
-import com.rokuality.core.exceptions.SessionNotStartedException;
-import com.rokuality.core.utils.SleepUtils;
 
 import org.testng.annotations.*;
 
@@ -27,10 +19,10 @@ import ru.yandex.qatools.allure.annotations.Features;
 
 public class XBoxTests {
 
-	private static final File ROKU_IMAGES_DIR = new File(String.join(File.separator, System.getProperty("user.dir"),
+	private static final File XBOX_IMAGES_DIR = new File(String.join(File.separator, System.getProperty("user.dir"),
 			"src", "test", "resources", "images", "xboximages") + File.separator);
 	private static final File DEMO_APP_PACKAGE = new File(
-			ROKU_IMAGES_DIR.getParentFile().getParent() + File.separator + "XBoxDebug.appxbundle");
+			XBOX_IMAGES_DIR.getParentFile().getParent() + File.separator + "XBoxDebug.appxbundle");
 
 	private static final String SERVER_URL = "http://localhost:7777";
 
@@ -60,10 +52,17 @@ public class XBoxTests {
 		// Indicates we want a Roku test session
 		capabilities.addCapability("Platform", "XBox");
 
-		// Location (path or url to a sideloadable zip)
-		//capabilities.addCapability("AppPackage", "https://rokualitypublic.s3.amazonaws.com/RokualityDemoApp.zip");
+		// The ip address of your harmony
+		capabilities.addCapability("HomeHubIPAddress", "192.168.1.41");
+		
+		// The name of your device as saved in harmony
+		capabilities.addCapability("DeviceName", "XboxOne");
 
-		// Your Roku device ip address.
+		// Location (path or url) to an appxbundle file
+		capabilities.addCapability("AppPackage", "https://rokualitypublic.s3.amazonaws.com/XBoxDebug.appxbundle");
+		capabilities.addCapability("App", "MTV");
+		
+		// Your Xbox device ip address.
 		capabilities.addCapability("DeviceIPAddress", "192.168.1.36");
 
 		// OPTIONAL A base image match similarity tolerance between 0 and 1
@@ -71,10 +70,11 @@ public class XBoxTests {
 
 		// OPTIONAL A forced image resolution size that all image captures are resized
 		// to (width/height).
-		capabilities.addCapability("ScreenSizeOverride", "1280x820");
+		capabilities.addCapability("ScreenSizeOverride", "1920x1080");
 
 		// OPTIONAL ocr module 
-		capabilities.addCapability("OCRType", "Tesseract");
+		capabilities.addCapability("OCRType", "GoogleVision");
+		capabilities.addCapability("GoogleCredentials", System.getProperty("user.home") + File.separator + "Service.json");
 
 		// OPTIONAL - enforce ocr case sensitivity. If not provided all words during ocr
 		// evaluation are forced to lowercase
@@ -85,33 +85,195 @@ public class XBoxTests {
 		return capabilities;
 	}
 
-	@Test(groups = { "XBox", "Debug" })
-	@Features("Roku")
-	public void installFromLocalFileTest() {
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void installFromUrlTest() {
 
+		// install app from url
 		DeviceCapabilities caps = setBaseCapabilities();
-		caps.addCapability("AppPackage", DEMO_APP_PACKAGE.getAbsolutePath());
-		caps.addCapability("App", "MTV");
-		caps.addCapability("OCRType", "GoogleVision");
-		caps.addCapability("GoogleCredentials", System.getProperty("user.home") + File.separator + "Service.json");
-		caps.addCapability("HomeHubIPAddress", "192.168.1.41");
-		caps.addCapability("DeviceName", "XboxOne");
+		caps.addCapability("AppPackage", "https://rokualitypublic.s3.amazonaws.com/XBoxDebug.appxbundle");
 
 		xboxDriver = new XBoxDriver(SERVER_URL, caps);
 
 		xboxDriver.options().setElementTimeout(15000);
 		xboxDriver.finder().findElement(By.Text("featured"));
 
-		xboxDriver.remote().pressButton(XBoxButton.DOWN_ARROW);
-		SleepUtils.sleep(2000);
-		//xboxDriver.remote().pressButton(XBoxButton.UP_ARROW);
-		SleepUtils.sleep(2000);
-		xboxDriver.remote().pressButton(XBoxButton.SELECT);
-		SleepUtils.sleep(4000);
-		xboxDriver.remote().pressButton(XBoxButton.BACK);
-		SleepUtils.sleep(5000);
-		System.out.println(xboxDriver.screen().getRecording());
+	}
 
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void installFromLocalFileTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		caps.addCapability("AppPackage", DEMO_APP_PACKAGE.getAbsolutePath());
+		caps.addCapability("App", "MTV");
+		
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+
+		xboxDriver.options().setElementTimeout(15000);
+		xboxDriver.finder().findElement(By.Text("featured"));
+
+	}
+
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void launchAlreadyInstalledApp() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		caps.removeCapability("AppPackage");
+		caps.addCapability("App", "MTV");
+		
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+
+		xboxDriver.options().setElementTimeout(15000);
+		xboxDriver.finder().findElement(By.Text("featured"));
+
+	}
+
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void findElementFromTextWithGoogleVisionTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		caps.addCapability("OCRType", "GoogleVision");
+		caps.addCapability("GoogleCredentials", System.getProperty("user.home") + File.separator + "Service.json");
+
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+
+		xboxDriver.options().setElementTimeout(15000);
+		Element element = xboxDriver.finder().findElement(By.Text("featured"));
+		System.out.println(element);
+		
+		Point elementLocation = element.getLocation();
+		Assert.assertTrue(elementLocation.x > 90 && elementLocation.x < 1100);
+		Assert.assertTrue(elementLocation.y > 450 && elementLocation.y < 460);
+
+		Dimension elementSize = element.getSize();
+		Assert.assertTrue(elementSize.width > 130 && elementSize.width < 145);
+		Assert.assertTrue(elementSize.height > 20 && elementSize.height < 30);
+
+		String elementText = element.getText();
+		Assert.assertEquals("Featured", elementText);
+
+		double confidence = element.getConfidence();
+		Assert.assertEquals(0.0, confidence); // confidence only for tesseract
+
+		Assert.assertTrue(!element.getElementID().isEmpty());
+		Assert.assertEquals(element.getSessionID(), xboxDriver.getSessionID());
+
+	}
+
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void findElementFromImageTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+		xboxDriver.options().setElementTimeout(15000);
+
+		for (By by : Arrays.asList(By.Image(XBOX_IMAGES_DIR.getAbsolutePath() + File.separator + "featured.png"), 
+				By.Image("https://dl.dropboxusercontent.com/s/36upxsx1cbhh3tp/featured.png"))) {
+					
+			Element element = xboxDriver.finder().findElement(by);
+			System.out.println(element);
+			Point elementLocation = element.getLocation();
+			Assert.assertTrue(elementLocation.x > 90 && elementLocation.x < 110);
+			Assert.assertTrue(elementLocation.y > 445 && elementLocation.y < 460);
+	
+			Dimension elementSize = element.getSize();
+			Assert.assertTrue(elementSize.width > 140 && elementSize.width < 150);
+			Assert.assertTrue(elementSize.height > 25 && elementSize.height < 35);
+	
+			double confidence = element.getConfidence();
+			Assert.assertTrue(confidence > 0.90);
+	
+			Assert.assertTrue(!element.getText().isEmpty());
+			Assert.assertTrue(!element.getElementID().isEmpty());
+			Assert.assertEquals(element.getSessionID(), xboxDriver.getSessionID());
+		}
+
+	}
+
+	@Test(groups = { "XBox" }, expectedExceptions = NoSuchElementException.class)
+	@Features("XBox")
+	public void elementNotFoundInTextTesseractTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		caps.addCapability("OCRType", "Tesseract");
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+		xboxDriver.finder().findElement(By.Text("not found"));
+
+	}
+
+	@Test(groups = { "XBox" }, expectedExceptions = NoSuchElementException.class)
+	@Features("XBox")
+	public void elementNotFoundInTextGoogleVisionTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+		xboxDriver.finder().findElement(By.Text("not found"));
+
+	}
+
+	@Test(groups = { "XBox" }, expectedExceptions = NoSuchElementException.class)
+	@Features("Xbox")
+	public void elementNotFoundImageTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+		xboxDriver.finder()
+				.findElement(By.Image(XBOX_IMAGES_DIR.getAbsolutePath() + File.separator + "helloworld.png"));
+
+	}
+
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void elementSetTimeoutTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+
+		xboxDriver.options().setElementTimeout(5000);
+		long startTime = System.currentTimeMillis();
+		Exception exception = null;
+		try {
+			xboxDriver.finder().findElement(By.Text("not here"));
+		} catch (NoSuchElementException nse) {
+			exception = nse;
+		}
+		Assert.assertNotNull(exception);
+
+		long endTime = System.currentTimeMillis();
+		long diffInSeconds = (endTime - startTime) / 1000;
+		System.out.println(diffInSeconds);
+		Assert.assertTrue(diffInSeconds >= 5 && diffInSeconds <= 7);
+
+	}
+
+	@Test(groups = { "XBox" })
+	@Features("XBox")
+	public void findElementInSubScreenTest() {
+
+		DeviceCapabilities caps = setBaseCapabilities();
+		xboxDriver = new XBoxDriver(SERVER_URL, caps);
+		xboxDriver.options().setElementTimeout(15000);
+		
+		Element element = xboxDriver.finder().findElement(By.Text("featured"));
+		System.out.println(element);
+
+		for (By by : Arrays.asList(By.Image(XBOX_IMAGES_DIR.getAbsolutePath() + File.separator + "featured.png"), By.Text("featured"))) {
+			xboxDriver.finder().findElement(by, 75, 410, 230, 90);
+
+			Exception exception = null;
+			try {
+				xboxDriver.finder().findElement(by, 500, 500, 100, 100);
+			} catch (NoSuchElementException nse) {
+				exception = nse;
+			}
+			Assert.assertNotNull(exception);
+		}
+		
 	}
 
 	
